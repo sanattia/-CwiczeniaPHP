fb1425a314fd56f95ddbffb0f9508e0ed491a7c1
Student <student@localhost>
2023-03-28 09:26:37 +0200

    :)

--- src/StringCalculator/StringCalculator.php
+++ src/StringCalculator/StringCalculator.php
@@ -5,11 +5,21 @@ namespace App\StringCalculator;
 
 class StringCalculator{
     public function add(string $input): int{
-
-    $numbers = $this->extractAddendsFromString($input);
+        if (!preg_match('/^(\d+([,\n])*\d+)?$/')){
+            throw new \ValueError('Invalid Input');
+        }
+        $numbers = $this->parseInput($input);
 
     return array_sum($numbers);
 }
+
+    private function parseInput(string $input): array{
+        $inputNormalized = $this->normalizeDelimiters($input);
+        return $this->extractAddendsFromString($inputNormalized);
+    }
+    private function normalizeDelimiters(string $input): string{
+        return str_replace(['/n'], ',', $input);
+    }
     private function extractAddendsFromString(string $input): array{
         $addends = explode(',', $input);
 
--- tests/StringCalculator/StringCalculatorTest.php
+++ tests/StringCalculator/StringCalculatorTest.php
@@ -5,6 +5,7 @@ use App\StringCalculator\StringCalculator;
 use PHPUnit\Framework\TestCase;
 
 use Generator;
+use ValueError;
 
 
 class StringCalculatorTest extends TestCase{
@@ -45,6 +46,33 @@ class StringCalculatorTest extends TestCase{
         yield 'two numbers: 6, 5' => [
             'numbers' => '6,5',
             'expected' => 11
+        ];
+        yield 'new line separator: 1,2\n3' => [
+            'numbers' => '1,2\n3',
+            'expected' => 6
+        ];
+    }
+
+    /**
+    * @dataProvider dataProviderForTestAddForIncorrectDelimitersPosition
+     */
+    public function testAddForIncorrectDelimitersPosition(string $input, string $expectedException): void{
+        //given
+        $this->expectException($expectedException);
+        $calculator = new StringCalculator();
+        $calculator ->add($input);
+        //when
+
+    }
+
+    public function  dataProviderForTestAddForIncorrectDelimitersPosition(): Generator{
+        yield 'incorrect delimiters position: 1, \n'=> [
+            'input' => '1,\n',
+            'exoectedException' => ValueError::class
+        ];
+        yield 'incorrect delimiters position: ,,1'=> [
+            'input' => ',,1',
+            'exoectedException' => ValueError::class
         ];
     }
 }
